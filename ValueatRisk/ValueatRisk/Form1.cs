83c99d807ddd5b1b35453cdc0febd1a118075ff8
Zsófia Utasi <zsofia.utasi@stud.uni-corvinus.hu>
2021-10-22 22:44:17 +0200

    Portfólió tárolása a memóriában

--- ValueatRisk/ValueatRisk/Form1.cs
+++ ValueatRisk/ValueatRisk/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ValueatRisk.Entities;
 
 namespace ValueatRisk
 {
@@ -14,12 +15,26 @@ namespace ValueatRisk
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
 
         public Form1()
         {
             InitializeComponent();
+            CreatePortfolio();
+
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
