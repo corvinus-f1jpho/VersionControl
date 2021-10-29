9fdfb769325e5f389d8af1a6b17a0e515159820e
Zsófia Utasi <zsofia.utasi@stud.uni-corvinus.hu>
2021-10-29 19:50:10 +0200

    Árfolyam lista létrehozása

--- ProofofConcept/ProofofConcept/Form1.cs
+++ ProofofConcept/ProofofConcept/Form1.cs
@@ -1,4 +1,5 @@
-﻿using ProofofConcept.MNBServiceReference;
+﻿using ProofofConcept.Entities;
+using ProofofConcept.MNBServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,10 +15,12 @@ namespace ProofofConcept
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
             Consume();
+            dataGridView1.DataSource = Rates;
         }
 
         void Consume()
