ec689306d018ea9647f195180d8ee1406565a68d
Zsófia Utasi <zsofia.utasi@stud.uni-corvinus.hu>
2021-10-29 18:25:50 +0200

    Webszolgáltatás hívása

--- ProofofConcept/ProofofConcept/Form1.cs
+++ ProofofConcept/ProofofConcept/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using ProofofConcept.MNBServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +22,19 @@ namespace ProofofConcept
 
         void Consume()
         {
-            MNbArfolyamServiceSoapClient
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            string result = response.GetExchangeRatesResult;
+            File.WriteAllText("export.xml", result);
         }
     }
 }
