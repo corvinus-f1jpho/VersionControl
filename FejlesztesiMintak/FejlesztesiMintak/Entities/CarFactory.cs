3073c6b53d9a9cd25ac037d34394486f63a2831f
Zsófia Utasi <zsofia.utasi@stud.uni-corvinus.hu>
2021-11-12 22:22:19 +0100

    Absztrakció felhasználása a gyárakban

--- FejlesztesiMintak/FejlesztesiMintak/Entities/CarFactory.cs
+++ FejlesztesiMintak/FejlesztesiMintak/Entities/CarFactory.cs
@@ -1,5 +1,4 @@
-﻿using FejlesztesiMintak.Abstractions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace FejlesztesiMintak.Entities
 {
-   public class BallFactory : IToyFactory
-{
-    public Toy CreateNew()
+    class CarFactory
     {
-        return new Ball();
     }
-}
 }
