c801d4419bb9d3431e988816992b41f2b7d3eb2f
Zsófia Utasi <zsofia.utasi@stud.uni-corvinus.hu>
2021-11-12 23:14:03 +0100

    Színválasztó gomb

--- FejlesztesiMintak/FejlesztesiMintak/Form1.Designer.cs
+++ FejlesztesiMintak/FejlesztesiMintak/Form1.Designer.cs
@@ -37,12 +37,18 @@ namespace FejlesztesiMintak
             this.btnSelectBall = new System.Windows.Forms.Button();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnColor = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
             this.mainPanel.AutoSize = true;
+            this.mainPanel.Controls.Add(this.button3);
+            this.mainPanel.Controls.Add(this.button2);
+            this.mainPanel.Controls.Add(this.btnColor);
             this.mainPanel.Controls.Add(this.lblNext);
             this.mainPanel.Controls.Add(this.btnSelectPresent);
             this.mainPanel.Controls.Add(this.btnSelectCar);
@@ -78,6 +84,7 @@ namespace FejlesztesiMintak
             this.btnSelectCar.TabIndex = 0;
             this.btnSelectCar.Text = "CAR";
             this.btnSelectCar.UseVisualStyleBackColor = true;
+            this.btnSelectCar.Click += new System.EventHandler(this.btnSelectCar_Click);
             // 
             // btnSelectBall
             // 
@@ -87,6 +94,7 @@ namespace FejlesztesiMintak
             this.btnSelectBall.TabIndex = 0;
             this.btnSelectBall.Text = "BALL";
             this.btnSelectBall.UseVisualStyleBackColor = true;
+            this.btnSelectBall.Click += new System.EventHandler(this.btnSelectBall_Click);
             // 
             // createTimer
             // 
@@ -99,6 +107,35 @@ namespace FejlesztesiMintak
             this.conveyorTimer.Enabled = true;
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Blue;
+            this.btnColor.Location = new System.Drawing.Point(208, 132);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(143, 38);
+            this.btnColor.TabIndex = 2;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.CausesValidation = false;
+            this.button2.Location = new System.Drawing.Point(368, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 38);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Yellow;
+            this.button3.Location = new System.Drawing.Point(368, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 38);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -124,6 +161,9 @@ namespace FejlesztesiMintak
         private System.Windows.Forms.Button btnSelectPresent;
         private System.Windows.Forms.Button btnSelectCar;
         private System.Windows.Forms.Button btnSelectBall;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnColor;
     }
 }
 
