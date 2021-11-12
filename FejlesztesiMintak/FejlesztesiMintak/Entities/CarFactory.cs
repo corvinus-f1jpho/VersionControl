c8762591d05e17fa095b62dc8300c8911e61b2af
Zsófia Utasi <zsofia.utasi@stud.uni-corvinus.hu>
2021-11-12 22:44:56 +0100

    Absztrakció felhasználása a futószalagban

--- FejlesztesiMintak/FejlesztesiMintak/Entities/CarFactory.cs
+++ FejlesztesiMintak/FejlesztesiMintak/Entities/CarFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FejlesztesiMintak.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace FejlesztesiMintak.Entities
 {
-    class CarFactory
+    public class CarFactory : IToyFactory
     {
+        public Toy CreateNew()
+        {
+            return new Car();
+        }
     }
 }
