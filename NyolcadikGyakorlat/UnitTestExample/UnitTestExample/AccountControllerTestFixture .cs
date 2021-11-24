837e196ef740d0ac5b23943ea9413fd8292cf78e
Zsófia Utasi <zsofia.utasi@stud.uni-corvinus.hu>
2021-11-25 00:02:24 +0100

    E-mail validálás teszt függvény

---
+++ NyolcadikGyakorlat/UnitTestExample/UnitTestExample/AccountControllerTestFixture .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nuint.Framework;
+
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class AccountControllerTestFixture
+{
+	
+		public void TestValidateEmail(string email, bool expectedResult)
+		{
+			// ...
+		}
+	
+}
